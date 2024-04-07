26d93e83b3db3f277d7ef926f637a256c16e2dcd
Roshan <romeoroshan@gmail.com>
2024-04-07 22:36:23 +0530

    user profile change

--- MobilePart/app/src/main/java/com/example/mysmartdoorapp/UserProfileActivity.java
+++ MobilePart/app/src/main/java/com/example/mysmartdoorapp/UserProfileActivity.java
@@ -3,12 +3,19 @@ package com.example.mysmartdoorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    TextView username, useremail, userpw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        username = findViewById(R.id.usertextname);
+        useremail = findViewById(R.id.usertextemail);
+        userpw = findViewById(R.id.usertextpw);
     }
 }
